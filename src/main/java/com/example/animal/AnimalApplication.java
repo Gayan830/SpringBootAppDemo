d064add312c3c9e1ad0a6e12d12c50d330615a01
Gayan830 <malshan95104@gmail.com>
2019-11-11 12:22:49 +0530

    adding Servlet Bean Removed @ServletComponent annotations. Add index.html

--- src/main/java/com/example/animal/AnimalApplication.java
+++ src/main/java/com/example/animal/AnimalApplication.java
@@ -1,15 +1,41 @@
 package com.example.animal;
 
+import com.example.animal.servlet.HelloFilter;
+import com.example.animal.servlet.HelloListener;
+import com.example.animal.servlet.HelloServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ServletComponentScan
+//@ServletComponentScan
 public class AnimalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnimalApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean getServletRegistrationBean(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new HelloServlet());
+        servletRegistrationBean.addUrlMappings("/helloServlet");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HelloFilter());
+        filterRegistrationBean.addUrlPatterns("/helloServlet");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HelloListener> getServletListenerRegistrationBean(){
+        ServletListenerRegistrationBean servletListenerRegistrationBean = new ServletListenerRegistrationBean(new HelloListener());
+        return servletListenerRegistrationBean;
+    }
 }
--- src/main/java/com/example/animal/servlet/HelloServlet.java
+++ src/main/java/com/example/animal/servlet/HelloServlet.java
@@ -1,12 +1,9 @@
 package com.example.animal.servlet;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/helloServlet")
 public class HelloServlet extends HttpServlet {
