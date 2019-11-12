63c5628b89bd0b0d6e9581754745d12167bf587f
Gayan830 <malshan95104@gmail.com>
2019-11-12 12:24:58 +0530

    Added Spring boot Uint Test.

---
+++ src/main/java/com/example/animal/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.example.animal.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+    public String getUser(){
+        return "Tom";
+    }
+}
--- src/test/java/com/example/animal/AnimalApplicationTests.java
+++ src/test/java/com/example/animal/AnimalApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.animal;
 
+import com.example.animal.service.UserService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class) //Integrate Spring with JUnit
+@SpringBootTest(classes=AnimalApplication.class)
 class AnimalApplicationTests {
 
+    @Resource
+    private UserService userService;
+
     @Test
-    void contextLoads() {
+    public void testGetUser() {
+        Assert.assertEquals(userService.getUser(),"Tom");
     }
 
 }
